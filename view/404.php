83d35b41d1d3562fa8261a058377f426dffd05d4
Geovani Nieswald <geovaninieswald@gmail.com>
2019-06-21 02:44:43 -0300

    Adicionando tela 404

--- view/404.php
+++ view/404.php
@@ -1,7 +1,13 @@
-<?php \Classes\ClassLayout::setHead('Ops! - Poligno News', 'Página não encontrada', FALSE, FALSE); ?>
+<?php \Classes\ClassLayout::setHead('Oops! - Poligno News', 'Página não encontrada'); ?>
 
-
-<h1>Página não encontrada</h1>
-
+<h1 class="p-3 centro topo">404 - Página não encontrada</h1>
+   
+    <div class="centro ">
+        <img class="image-fluid" src="<?php echo DIRIMG ?>404.gif" alt="Gif Sorry">
+        <br>
+        <div class="topo">
+            <p class="p-4 tp404">Desculpe-nos, mas a página a qual você tentou acessar não foi encontrada, você pode voltar a página principal clicando <a href="home" class="p404">aqui</a>.</p>
+        </div>
+    </div>
 
 <?php \Classes\ClassLayout::setFooter(); ?>
